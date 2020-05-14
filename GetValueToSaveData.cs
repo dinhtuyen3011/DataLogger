229eaeea8162c3c0d5977b7f02303925ace3b619
Dinh Tuyen Nguyen <dinhtuyen3011@gmail.com>
2020-05-14 08:55:10 +0700

    init commit

---
+++ GetValueToSaveData.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataLogger
+{
+    public partial class GetValueToSaveData : Form
+    {
+        
+        public GetValueToSaveData()
+        {
+            InitializeComponent();
+        }
+        public string value { get; private set; }
+        public double timeSave { get; private set; }
+        public DateTime timeToSave { get; private set; }
+
+        private void days_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsNumber(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                MessageBox.Show("Mời nhập lại");
+                e.Handled = true;
+            }
+        }
+
+        private void hours_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsNumber(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                MessageBox.Show("Mời nhập lại");
+                e.Handled = true;
+            }
+        }
+
+        private void minutes_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsNumber(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                MessageBox.Show("Mời nhập lại");
+                e.Handled = true;
+            }
+        }
+
+        private void seconds_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsNumber(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                MessageBox.Show("Mời nhập lại");
+                e.Handled = true;
+            }
+        }
+
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void GetValueToSaveData_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+            if((hours.Text != ""))
+            {
+                    int a = Convert.ToInt32(hours.Text);
+                    if (a > 23)
+                    {
+                        hours.Text = "";
+                        if (MessageBox.Show("", "Nhập lại giờ", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                        {
+                            e.Cancel = true;
+                        }
+                        else
+                            e.Cancel = false;
+                    }
+            }
+            if ((minutes.Text != ""))
+            {
+                int b = Convert.ToInt32(minutes.Text);
+                if (b > 59)
+                {
+                    minutes.Text = "";
+                    if (MessageBox.Show("", "Nhập lại phút", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    {
+                        e.Cancel = true;
+                    }
+                    else
+                        e.Cancel = false;
+                }
+            }
+            if ((seconds.Text != ""))
+            {
+                int b = Convert.ToInt32(seconds.Text);
+                if (b > 59)
+                {
+                    seconds.Text = "";
+                    if (MessageBox.Show("", "Nhập lại giây", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    {
+                        e.Cancel = true;
+                    }
+                    else
+                        e.Cancel = false;
+                }
+            }
+            if ((days.Text == "") && (hours.Text == "") && (minutes.Text == "") && (seconds.Text == ""))
+            {
+                if (MessageBox.Show("Bạn chưa điền giá trị", "Tiếp tục", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    e.Cancel = false;
+                }
+            }
+        }
+        private void GetValueToSaveData_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            int ds = 0;
+            int hs = 0;
+            int ms = 0;
+            int s = 0;
+            if ((days.Text == "")|| (days.Text == "0"))
+            {
+                days.Text = "00";
+                
+            }
+            else
+            {
+                ds = int.Parse(days.Text);
+                days.Text = days.Text + "d";
+                
+            }
+            if ((hours.Text == "")|| (hours.Text == "0"))
+            {
+                hours.Text = "00";
+            }
+            else
+            {
+                hs = int.Parse(hours.Text);
+                hours.Text = hours.Text + "h";
+                
+            }
+            if ((minutes.Text == "")|| minutes.Text == "0")
+            {
+                minutes.Text = "00";
+            }
+            else
+            {
+                ms = int.Parse(minutes.Text);
+                minutes.Text = minutes.Text + "m";
+                
+            }
+            if ((seconds.Text == "")|| (seconds.Text == "0"))
+            {
+                seconds.Text = "00";
+            }
+            else
+            {
+                s = int.Parse(seconds.Text);
+                seconds.Text = seconds.Text + "s";
+                
+            }
+            value = days.Text + hours.Text + minutes.Text + seconds.Text ;
+            timeSave = ds * 86400 + hs * 3600 + ms * 60 + s;
+
+        }
+    }
+}
---
+++ oldDataForm.cs
@@ -0,0 +1,223 @@
+﻿using DataLogger.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace DataLogger
+{
+    public partial class oldDataForm : Form
+    {
+        Point? prevPosition = null;
+        ToolTip tooltip = new ToolTip();
+        private readonly static string CHANEL1 = "Chanel 1";
+        private readonly static string CHANEL2 = "Chanel 2";
+        private readonly static string CHANEL3 = "Chanel 3";
+        private readonly static string CHANEL4 = "Chanel 4";
+
+        private List<string> chanels = new List<string>() { CHANEL1, CHANEL2, CHANEL3, CHANEL4 };
+
+        private List<OldData> oldDatas = new List<OldData>();
+
+        public oldDataForm()
+        {
+            InitializeComponent();
+            chart2.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.Gray;
+            chart2.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = ChartDashStyle.NotSet;
+
+            chart2.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.Gray;
+            chart2.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.DashDotDot;
+            chart2.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart2.ChartAreas[0].AxisX.ScrollBar.Enabled = false;
+            chart2.ChartAreas[0].CursorX.AutoScroll = true;
+            chart2.ChartAreas[0].CursorX.IsUserEnabled = true;
+            chart2.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart2.ChartAreas[0].CursorX.Interval = 1;
+            chart2.ChartAreas[0].CursorX.LineColor = Color.Gray;
+            chart2.ChartAreas[0].AxisX.ScaleView.MinSize = 1;
+            chart2.ChartAreas[0].AxisX.ScaleView.SmallScrollMinSize = 1;
+            chart2.ChartAreas[0].AxisX.Title = "Mẫu";
+
+            chart2.ChartAreas[0].AxisY.Title = "Điện áp (V )";
+            chart2.ChartAreas[0].AxisX.MajorTickMark.Enabled = false;
+            chart2.ChartAreas[0].AxisY.MajorTickMark.Enabled = false;
+            chart2.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            chart2.ChartAreas[0].AxisY.ScrollBar.Enabled = false;
+            chart2.ChartAreas[0].CursorY.AutoScroll = true;
+            chart2.ChartAreas[0].CursorY.IsUserEnabled = true;
+            chart2.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            chart2.ChartAreas[0].CursorY.Interval = 0.001;
+            chart2.ChartAreas[0].CursorY.LineColor = Color.Gray;
+            chart2.MouseWheel += chart1_MouseWheels;
+            CheckForIllegalCrossThreadCalls = false;
+            
+        }
+        private void chart1_MouseWheels(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Delta < 0)
+                {
+                    chart2.ChartAreas[0].AxisX.ScaleView.ZoomReset();
+                    chart2.ChartAreas[0].AxisY.ScaleView.ZoomReset();
+                }
+                //if (e.Delta > 0)
+                //{
+                //    double xMin = chart1.ChartAreas[0].AxisX.ScaleView.ViewMinimum;
+                //    double xMax = chart1.ChartAreas[0].AxisX.ScaleView.ViewMaximum;
+                //    double yMin = chart1.ChartAreas[0].AxisY.ScaleView.ViewMinimum;
+                //    double yMax = chart1.ChartAreas[0].AxisY.ScaleView.ViewMaximum;
+
+                //    double posXStart = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 4;
+                //    double posXFinish = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 4;
+                //    double posYStart = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 4;
+                //    double posYFinish = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 4;
+
+                //    chart1.ChartAreas[0].AxisX.ScaleView.Zoom(posXStart, posXFinish);
+                //    chart1.ChartAreas[0].AxisY.ScaleView.Zoom(posYStart, posYFinish);
+                //}
+            }
+            catch
+            {
+            }
+
+            // mouseMove
+            var pos = e.Location;
+            if (prevPosition.HasValue && pos == prevPosition.Value)
+                return;
+            tooltip.RemoveAll();
+            prevPosition = pos;
+            var results = chart2.HitTest(pos.X, pos.Y, false, ChartElementType.DataPoint);
+            foreach (var result in results)
+            {
+                if (result.ChartElementType == ChartElementType.DataPoint)
+                {
+                    var prop = result.Object as DataPoint;
+                    if (prop != null)
+                    {
+                        var pointXPixel = result.ChartArea.AxisX.ValueToPixelPosition(prop.XValue);
+                        var pointYPixel = result.ChartArea.AxisY.ValueToPixelPosition(prop.YValues[0]);
+                        var channel = result.Series.ToString();
+                        var y_value = Math.Round(prop.YValues[0], 3);
+                        if (Math.Abs(pos.X - pointXPixel) < 2 &&
+                            Math.Abs(pos.Y - pointYPixel) < 2)
+                        {
+                            tooltip.Show(
+                                channel.Remove(0, 7) + ", Điểm :" + prop.XValue + ", Giá trị điện áp:" + y_value,
+                                this.chart2, pos.X, pos.Y - 15);
+                        }
+                    }
+                }
+            }
+        }
+        private readonly string _fullPath = "..\\..\\Files\\8_5_2020_7_6_DataLogger.csv";
+       
+        private void oldDataForm_Load(object sender, EventArgs e)
+        {
+            readCSV();
+            
+            chart2.Series.Clear();
+
+            chanels.ForEach(c =>
+            {
+                chart2.Series.Add(c);
+                chart2.Series[c].ChartType = SeriesChartType.Line;
+                chart2.Series[c].Color = c == CHANEL1 ? Color.Red : c == CHANEL2 ? Color.Green : c == CHANEL3 ? Color.Blue : Color.Gray;
+                chart2.Series[c].IsVisibleInLegend = false;
+            });
+        }
+
+        private void readCSV()
+        {
+            OpenFileDialog getFileDialog = new OpenFileDialog();
+            
+            getFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            getFileDialog.Title = "Select File Location";
+            getFileDialog.ShowDialog();
+            List<string[]> rows = File.ReadAllLines(getFileDialog.FileName).Select(x => x.Split(',')).ToList();
+
+            rows.RemoveAt(0);
+
+            for(int i = 0; i < rows.Count(); i++)
+            {
+                for(int j = 1; j < rows[i].Length; j ++)
+                {
+                    oldDatas.Add(new OldData()
+                    {
+                        Ms = int.Parse(rows[i][0]),
+                        Chanel = chanels[j - 1],
+                        Value = float.Parse(rows[i][j])
+                    });
+                }
+            }
+        }
+
+        private void draw(string chanel = "", bool all = false)
+        {
+            if(!all && chanel != "")
+            {
+                var data = oldDatas.Where(d => d.Chanel == chanel).ToList();
+                data.ForEach(d =>
+                {
+                    
+                    chart2.Series[d.Chanel].Points.AddXY(d.Ms, d.Value);
+                });
+            } 
+            else {
+                oldDatas.ForEach(d =>
+                {
+                    
+                    chart2.Series[d.Chanel].Points.AddXY(d.Ms, d.Value);
+                });
+            }
+
+        }
+
+        private void channel1Displays_CheckStateChanged(object sender, EventArgs e)
+        {
+            allDisplays.Checked = false;
+            draw(CHANEL1);
+        }
+
+        private void channel2Displays_CheckStateChanged(object sender, EventArgs e)
+        {
+            allDisplays.Checked = false;
+            draw(CHANEL2);
+        }
+
+        private void channel3Displays_CheckStateChanged(object sender, EventArgs e)
+        {
+            allDisplays.Checked = false;
+            draw(CHANEL3);
+        }
+
+        private void channel4Displays_CheckStateChanged(object sender, EventArgs e)
+        {
+            allDisplays.Checked = false;
+            draw(CHANEL4);
+        }
+
+        private void allDisplays_CheckStateChanged(object sender, EventArgs e)
+        {
+            channel1Displays.Checked = false;
+            channel2Displays.Checked = false;
+            channel3Displays.Checked = false;
+            channel4Displays.Checked = false;
+            chart2.Series[0].Points.Clear();
+            chart2.Series[1].Points.Clear();
+            chart2.Series[2].Points.Clear();
+            chart2.Series[3].Points.Clear();
+            draw("", true);
+        }
+
+        
+    }
+}
